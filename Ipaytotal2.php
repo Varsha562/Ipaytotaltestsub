bf55fa7689fe0be1e57bae0181d39dca27b92ca3
Varsha562 <68768362+Varsha562@users.noreply.github.com>
2020-12-04 17:53:24 +0530

    Update Ipaytotal2.php

--- Ipaytotal2.php
+++ Ipaytotal2.php
@@ -30,6 +30,8 @@ session_start();
         <link href="https://ipaytotal.solutions/newTheme/assets/lib/select2/css/select2.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://ipaytotal.solutions/newTheme/assets/css/dashforge.css">
         <link rel="stylesheet" href="https://ipaytotal.solutions/newTheme/assets/css/dashforge.auth.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         <style type="text/css">
             .transaction {
                 position: relative;
@@ -220,5 +222,5 @@ session_start();
                 </nav>
             </div>
         </footer>
-    </body>
+        </body>
 </html>
