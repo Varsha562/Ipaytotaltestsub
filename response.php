d51ad7ade56f646304feaf0cb6ded1145cdf1969
Varsha562 <68768362+Varsha562@users.noreply.github.com>
2020-12-04 09:44:12 +0530

    Update response.php

--- response.php
+++ response.php
@@ -1,5 +1,20 @@
 <?php
 
-// after 3d complete, here comes response in $_GET parameter
-echo '<pre>';print_r($_GET);exit();
+$data = ['order_id' => $_GET["order_id"],
+          'sulte_apt_no' => $_GET["sulte_apt_no"]
+        ];
+if($_GET['status']=="success")
+{
+$url ='https://ipaytotal.solutions/response?status=success&message=Your%20transaction%20was%20success&';
+$url .= http_build_query($data);
+header('Location :'.$url);
+}
+else
+  if($_GET['status']=="fail")
+  {
+$url = 'https://ipaytotal.solutions/response?status=fail&message=Activity%20limit%20exceeded.';
+$url .= http_build_query($data);
+header('Location :'.$url);
+}
+exit();
 ?>
